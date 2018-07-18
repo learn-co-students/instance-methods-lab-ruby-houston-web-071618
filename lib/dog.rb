86175e75370bfdd919c40e819852e458c009e157
pandasandeggs <theteatreehouse@gmail.com>
2018-07-18 12:37:17 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+
+  def bark
+    puts "Woof!"
+  end
+  # makes dog bark
+
+  def sit
+    puts "The Dog is sitting"
+  end
+  #makes dog sit
+
+end
+
+
+class Person
+
+  def talk
+    puts "Hello World!"
+  end
+
+  def walk
+    puts "The Person is walking"
+  end
+
+end
